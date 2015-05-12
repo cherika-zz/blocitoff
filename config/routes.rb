7676ed877e358d456c5e5475f8e5b6b967174b76
Selene <hakobyan.sn@gmail.com>
2015-05-11 22:47:35 -0700

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'welcome#index'
+  root "application#hello"
+  # root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
